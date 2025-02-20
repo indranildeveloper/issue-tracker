7264f0f1a850b6d7b851abdc5ccc235fdca98fd0
Indranil Halder <indranilhalder.dev@gmail.com>
2025-02-20 16:19:43 +0530

    Add markdown editor

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import { FC } from "react";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+// TODO: Refactor and extract common components
 
 const NewIssuePage: FC = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title" />
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
