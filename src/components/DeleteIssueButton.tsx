47eaae227e99c20d1e9c614758f0e6690f2bc4fc
Indranil Halder <indranilhalder.dev@gmail.com>
2025-02-21 16:57:00 +0530

    Delete an issue from ui

--- src/components/DeleteIssueButton.tsx
+++ src/components/DeleteIssueButton.tsx
@@ -1,36 +1,49 @@
 "use client";
 
 import { FC } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { DeleteIssueButtonProps } from "@/interfaces/DeleteIssueButtonProps";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const DeleteIssueButton: FC<DeleteIssueButtonProps> = ({ issueId }) => {
+  const router = useRouter();
+
+  const handleDeleteIssue = async () => {
+    await axios.delete(`/api/issues/${issueId}`);
+    router.push("/issues");
+    router.refresh();
+  };
+
   return (
-    <>
-      <AlertDialog.Root>
-        <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
-        </AlertDialog.Trigger>
-        <AlertDialog.Content>
-          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-          <AlertDialog.Description>
-            Are you sure you want to delete this issue? This action can not be
-            undone.
-          </AlertDialog.Description>
-          <Flex mt="4" gap="3">
-            <AlertDialog.Cancel>
-              <Button color="gray" variant="soft">
-                Cancel
-              </Button>
-            </AlertDialog.Cancel>
-            <AlertDialog.Action>
-              <Button color="red">Delete Issue</Button>
-            </AlertDialog.Action>
-          </Flex>
-        </AlertDialog.Content>
-      </AlertDialog.Root>
-    </>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action can not be
+          undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button
+              color="red"
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              onClick={handleDeleteIssue}
+            >
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 export default DeleteIssueButton;
