2c9d5d01fd9a3b3c18d8b4006b31b2c8e31fedce
Indranil Halder <indranilhalder.dev@gmail.com>
2025-02-25 14:46:05 +0530

    Show toast notifications

--- src/components/AssigneeSelect.tsx
+++ src/components/AssigneeSelect.tsx
@@ -4,6 +4,7 @@ import { FC } from "react";
 import axios from "axios";
 import { Avatar, Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
+import toast, { Toaster } from "react-hot-toast";
 import { User } from "@prisma/client";
 import Skeleton from "./Skeleton";
 import { AssigneeSelectProps } from "@/interfaces";
@@ -26,38 +27,42 @@ const AssigneeSelect: FC<AssigneeSelectProps> = ({ issue }) => {
   if (error) return null;
 
   return (
-    <Select.Root
-      onValueChange={(userId) => {
-        axios
-          .patch(`/api/issues/${issue.id}`, {
-            assignedToUserId: userId === "unassigned" ? null : userId,
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }}
-      defaultValue={issue.assignedToUserId || "unassigned"}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="unassigned">Unassigned</Select.Item>
-          <Select.Separator />
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id} className="mb-1">
-              <Avatar
-                src={user.image as string}
-                fallback={user.name!.slice(0, 1)}
-                radius="full"
-                size="1"
-              />
-              <span className="ml-3">{user.name}</span>
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        onValueChange={(userId) => {
+          axios
+            .patch(`/api/issues/${issue.id}`, {
+              assignedToUserId: userId === "unassigned" ? null : userId,
+            })
+            .catch((error) => {
+              console.log(error);
+              toast.error("Changes could not be saved at this moment!");
+            });
+        }}
+        defaultValue={issue.assignedToUserId || "unassigned"}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            <Select.Separator />
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id} className="mb-1">
+                <Avatar
+                  src={user.image as string}
+                  fallback={user.name!.slice(0, 1)}
+                  radius="full"
+                  size="1"
+                />
+                <span className="ml-3">{user.name}</span>
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
