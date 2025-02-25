57ef16abd64884a9bcc86cb3f6bfcbe070479064
Indranil Halder <indranilhalder.dev@gmail.com>
2025-02-25 21:55:00 +0530

    Fix filtering bugs

--- src/components/IssueStatusFilter.tsx
+++ src/components/IssueStatusFilter.tsx
@@ -1,20 +1,30 @@
 "use client";
 
 import { FC } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Select } from "@radix-ui/themes";
 import { statuses } from "@/constants/issueStatuses";
 
 const IssueStatusFilter: FC = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   const handleStatusValueChange = (status: string) => {
-    const query = status !== "all" ? `?status=${status}` : "";
+    const params = new URLSearchParams();
+
+    if (status) params.append("status", status);
+    if (searchParams.get("orderBy"))
+      params.append("orderBy", searchParams.get("orderBy")!);
+
+    const query = params.size ? `?${params.toString()}` : "";
     router.push(`/issues/list/${query}`);
   };
 
   return (
-    <Select.Root onValueChange={handleStatusValueChange}>
+    <Select.Root
+      defaultValue={searchParams.get("status") ?? "all"}
+      onValueChange={handleStatusValueChange}
+    >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
